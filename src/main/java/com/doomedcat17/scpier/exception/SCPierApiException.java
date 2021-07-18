17a189e0cec4da275da9ceecdb2d2956b212202f
doomedcat17 <doomedcat17@outlook.com>
2021-07-19 00:44:43 +0200

    - SCPierApiException.java created Now SCPierApiInternalException and SCPWikiContentNotFound extends SCPierApiException.java for easier handling

---
+++ src/main/java/com/doomedcat17/scpier/exception/SCPierApiException.java
@@ -0,0 +1,8 @@
+package com.doomedcat17.scpier.exception;
+
+public class SCPierApiException extends Exception{
+
+    public SCPierApiException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/doomedcat17/scpier/exception/ScpTaleMapperException.java
+++
@@ -1,7 +0,0 @@
-package com.doomedcat17.scpier.exception;
-
-public class ScpTaleMapperException extends RuntimeException {
-    public ScpTaleMapperException(String message) {
-        super(message);
-    }
-}
