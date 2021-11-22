6f7c002e52e42bf4a1326fb20acf69892046bc49
doomedcat17 <doomedcat17@outlook.com>
2021-11-22 22:26:25 +0100

    TextScraper refactor

--- src/test/java/com/doomedcat17/scpier/testbox/JSONWriter.java
+++ src/test/java/com/doomedcat17/scpier/testbox/JSONWriter.java
@@ -13,7 +13,7 @@ public class JSONWriter {
         ScpFoundationDataProvider scpFoundationDataProvider = new ScpFoundationDataProvider();
         try {
             ObjectMapper objectMapper = new ObjectMapper();
-            ScpWikiData scp = scpFoundationDataProvider.getScpWikiData("scp-asdasdads", SCPBranch.ENGLISH, SCPLanguage.ENGLISH);
+            ScpWikiData scp = scpFoundationDataProvider.getScpWikiData("scp-173", SCPBranch.ENGLISH, SCPLanguage.ENGLISH);
             System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(scp));
 
         } catch (Exception e) {
