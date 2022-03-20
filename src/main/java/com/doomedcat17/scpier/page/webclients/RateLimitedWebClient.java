4ca809e690bf76043ba98e3d8806a1c6f6cfff3b
Piotr Bojczewski <piotr200310@outlook.com>
2022-03-20 20:46:19 +0100

    add Polyfill support

--- src/main/java/com/doomedcat17/scpier/page/webclients/RateLimitedWebClient.java
+++ src/main/java/com/doomedcat17/scpier/page/webclients/RateLimitedWebClient.java
@@ -4,16 +4,16 @@ import com.gargoylesoftware.htmlunit.*;
 import com.gargoylesoftware.htmlunit.javascript.SilentJavaScriptErrorListener;
 
 import java.io.IOException;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 import java.util.logging.Level;
 
 public class RateLimitedWebClient extends WebClient {
 
     private long requestCounter = 0;
 
-    private LocalTime startTime;
+    private LocalDateTime startTime;
 
-    private LocalTime endTime;
+    private LocalDateTime endTime;
 
     private final long TIME_PERIOD;
 
@@ -22,14 +22,14 @@ public class RateLimitedWebClient extends WebClient {
     @Override
     public <P extends Page> P getPage(String url) throws IOException {
         try {
-            if (startTime == null) startTime = LocalTime.now();
+            if (startTime == null) startTime = LocalDateTime.now();
             if (endTime == null) endTime = startTime.plusSeconds(TIME_PERIOD);
-            boolean reachedCap = LocalTime.now().isBefore(endTime) && requestCounter == REQUEST_CAP;
+            boolean reachedCap = LocalDateTime.now().isBefore(endTime) && requestCounter == REQUEST_CAP;
             while (reachedCap) {
-                reachedCap = LocalTime.now().isBefore(endTime) && requestCounter == REQUEST_CAP;
+                reachedCap = LocalDateTime.now().isBefore(endTime) && requestCounter == REQUEST_CAP;
             }
-            if (LocalTime.now().isAfter(endTime)) {
-                startTime = LocalTime.now();
+            if (LocalDateTime.now().isAfter(endTime)) {
+                startTime = LocalDateTime.now();
                 endTime = startTime.plusSeconds(TIME_PERIOD);
                 requestCounter = 0;
             }
@@ -42,6 +42,7 @@ public class RateLimitedWebClient extends WebClient {
 
     public RateLimitedWebClient(long timePeriod, long requestCap) {
         super(BrowserVersion.CHROME);
+        super.getOptions().setFetchPolyfillEnabled(true);
         TIME_PERIOD = timePeriod;
         REQUEST_CAP = requestCap;
         super.getOptions().setJavaScriptEnabled(true);
