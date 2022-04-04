cb155a4697e4dabc8f5582225e5cbec46e14457d
Piotr Bojczewski <piotr200310@outlook.com>
2022-04-04 22:51:57 +0200

    refactor iframe providing

---
+++ src/main/java/com/doomedcat17/scpier/page/html/document/provider/iframe/IframeContentProvider.java
@@ -0,0 +1,78 @@
+package com.doomedcat17.scpier.page.html.document.provider.iframe;
+
+import com.doomedcat17.scpier.data.files.ResourcesProvider;
+import com.doomedcat17.scpier.exception.page.html.document.provider.IframeContentProviderException;
+import com.doomedcat17.scpier.page.WikiContent;
+import com.doomedcat17.scpier.page.html.document.cleaner.WikiContentCleaner;
+import com.doomedcat17.scpier.page.html.document.preset.executor.PresetExecutor;
+import com.doomedcat17.scpier.page.html.document.provider.DefaultWikiPageProvider;
+import com.doomedcat17.scpier.page.html.document.provider.iframe.mapper.IframeMapper;
+import com.doomedcat17.scpier.page.html.document.provider.iframe.mapper.IframeMapperProvider;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.Objects;
+import java.util.Set;
+
+public class IframeContentProvider {
+
+    private final WikiContentCleaner wikiContentCleaner;
+    private final DefaultWikiPageProvider defaultWikiPageProvider;
+    private final PresetExecutor presetExecutor;
+
+    public IframeContentProvider(WikiContentCleaner wikiContentCleaner, DefaultWikiPageProvider defaultWikiPageProvider, PresetExecutor presetExecutor) {
+        this.wikiContentCleaner = wikiContentCleaner;
+        this.defaultWikiPageProvider = defaultWikiPageProvider;
+        this.presetExecutor = presetExecutor;
+    }
+
+    public void includeIframesContent(WikiContent wikiContent) throws IframeContentProviderException {
+        try {
+            Elements iframes = wikiContent.getContent().select("iframe");
+            iframes.forEach(element -> replaceWithIframeContent(element, wikiContent));
+        } catch (Exception e) {
+            throw new IframeContentProviderException(e);
+        }
+    }
+
+    private void replaceWithIframeContent(Element iframe, WikiContent wikiContent) {
+        if (isTrash(iframe)) {
+            iframe.remove();
+        } else {
+            try {
+            IframeMapper iframeMapper =
+                    IframeMapperProvider.getMapper(iframe, defaultWikiPageProvider, presetExecutor);
+            Element iframeContent = iframeMapper.map(iframe, wikiContent);
+            if (Objects.isNull(iframeContent) || iframeContent.children().isEmpty()) {
+                iframe.remove();
+            } else {
+                WikiContent iframeWikiContent = new WikiContent(wikiContent);
+                iframeWikiContent.setContent(iframeContent);
+                includeIframesContent(iframeWikiContent);
+                iframeContent = iframeWikiContent.getContent();
+                wikiContentCleaner.removeTrashNodes(iframeContent);
+                replaceIframeWithItsContent(iframe, iframeContent);
+            }
+            } catch (Exception e) {
+                iframe.remove();
+            }
+        }
+    }
+
+    private boolean isTrash(Element iframeElement) {
+        Set<String> definitions = ResourcesProvider.getTrashIframesDefinitions();
+        return definitions.stream().anyMatch(iframeElement::is);
+    }
+
+
+    private void replaceIframeWithItsContent(Element iframe, Element iframeContent) {
+        if (iframe.parent().childrenSize() == 1) {
+            iframeContent.children().forEach(element -> iframe.parent().after(element));
+            iframe.parent().remove();
+        } else {
+            iframeContent.children().forEach(iframe::after);
+            iframe.remove();
+        }
+    }
+
+}
