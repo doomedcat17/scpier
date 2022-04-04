eedae3d636d2a303dde08b3ff622ad7631232639
Piotr Bojczewski <piotr200310@outlook.com>
2022-04-05 00:15:34 +0200

    remove WikiPresetNotFoundException.java

--- src/main/java/com/doomedcat17/scpier/exception/page/html/document/preset/WikiPresetNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.doomedcat17.scpier.exception.page.html.document.preset;
-
-public class WikiPresetNotFoundException extends RuntimeException {
-
-    public WikiPresetNotFoundException(String presetName) {
-        super("Preset not found for "+presetName);
-    }
-
-}
